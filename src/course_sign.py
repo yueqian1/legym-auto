97b98c04a8b241ddff3d501297619a8ca0309bb0
Foreverddb <835236331@qq.com>
2022-04-26 18:22:32 +0800

    增加课程自动签到

---
+++ src/course_sign.py
@@ -0,0 +1,33 @@
+import time
+
+import legym_api
+import datetime
+import os
+
+username = os.environ.get('USERNAME')
+password = os.environ.get('PASSWORD')
+keyword = os.environ.get('COURSEKEYWORD')
+
+i = 0
+
+
+def listen_course(course_id):
+    interval = 20
+    while True:
+        global i
+        if i >= 360 or user.signin_course(course_id):
+            break
+        i += 1
+        try:
+            curr_time = datetime.datetime.now()
+            print(datetime.datetime.strftime(curr_time, '%Y-%m-%d %H:%M:%S') + ':' + course_id)
+            time.sleep(interval)
+        except:
+            time.sleep(interval)
+            continue
+
+
+user = legym_api.login(username, password)
+for course in user.courses:
+    if keyword in course['name']:
+        listen_course(course['id'])
--- src/legym_api.py
+++ src/legym_api.py
@@ -4,19 +4,23 @@ import requests
 base_url = 'https://cpes.legym.cn'
 
 login_url = base_url + '/authorization/user/manage/login'
+get_current_url = base_url + '/education/semester/getCurrent'
 get_activity_url = base_url + '/education/app/activity/getActivityList'
+get_course_url = base_url + '/education/course/app/forStudent/list'
+get_course_info_url = base_url + '/education/course/app/forStudent/attainability/info'
 signup_activity_url = base_url + '/education/app/activity/signUp'
 signin_activity_url = base_url + '/education/activity/app/attainability/sign'
+signin_course_url = base_url + '/education/course/app/forStudent/sign'
 
 headers = {
     'Content-Type': 'application/json'
 }
 
 
-def req(method, url, headers, data, error_text=''):
-    response = requests.request(method=method, url=url, headers=headers, data=data)
+def req(method, url, headers, data=None, error_text='', params=None):
+    response = requests.request(method=method, url=url, headers=headers, data=data, params=params)
     if response.status_code != 200:
-        print(response.text)
+        print(error_text + response.text)
         raise Exception(error_text)
     else:
         return json.loads(response.text)
@@ -35,10 +39,19 @@ def login(username, password):
 class User:
     def __init__(self, access_token, user_id):
         self.activities = []
+        self.courses = []
+        self.current = {}
         self.headers = headers
         self.access_token = access_token
         self.headers['Authorization'] = 'Bearer ' + access_token
         self.user_id = user_id
+        self.get_current()
+        self.get_activities()
+        self.get_courses()
+
+    def get_current(self):
+        response = req(method='GET', url=get_current_url, headers=self.headers, error_text='获取本周课程信息失败')
+        self.current = response['data']
 
     def get_activities(self):
         payload = json.dumps({
@@ -53,6 +66,18 @@ class User:
         response = req(method='POST', url=get_activity_url, headers=self.headers, data=payload, error_text='获取活动列表失败')
         self.activities = response['data']['items']
 
+    def get_courses(self):
+        response = req(method='GET', url=get_course_url, headers=self.headers, error_text='获取课程列表失败')
+        self.courses = response['data']
+
+    def get_course_info(self, course_id):
+        params = {
+            'courseId': course_id
+        }
+        response = req(method='GET', url=get_course_info_url, headers=self.headers, params=params,
+                       error_text='获取课程信息失败')
+        print(response)
+
     def signup_activities(self, keyword):
         for activity in self.activities:
             if keyword in activity['name']:
@@ -61,6 +86,7 @@ class User:
                 })
                 response = req(method='POST', url=signup_activity_url, headers=self.headers, data=payload,
                                error_text='活动报名失败')
+                print('活动报名：' + activity['name'] + json.dumps(response))
 
     def signin_activities(self, keyword):
         for activity in self.activities:
@@ -75,4 +101,24 @@ class User:
                 })
                 response = req(method='PUT', url=signin_activity_url, headers=self.headers, data=payload,
                                error_text='活动签到失败')
-                print(response)
+                print('活动签到：' + activity['name'] + json.dumps(response))
+
+    def signin_course(self, course_id):
+        try:
+            payload = json.dumps({
+                'attainabilityType': "0",
+                'courseId': course_id,
+                'weekNumber': self.current['weekIndex'],
+                'startSignNumber': 1,
+                'pageType': 'course',
+                'userId': self.user_id
+            })
+            response = req(method='PUT', url=signin_course_url, headers=self.headers, data=payload,
+                           error_text='活动签到失败')
+            print('课程签到：' + course_id + json.dumps(response))
+            if '成功' in response['message']:
+                return True
+            else:
+                return False
+        except:
+            return False
